5e130083fc3d8730cb6e255ff81f4e0986c0c067
f.sousa <ng.dev.sousa@outlook.com>
2021-03-28 20:27:56 -0300

    Add get_user_by_id to UsersController

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,5 +1,7 @@
 defmodule Rockelivery do
   alias Rockelivery.Users.Create, as: UserCreate
+  alias Rockelivery.Users.Get, as: GetUserByID
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate get_user_by_id(id), to: GetUserByID, as: :by_id
 end
