f180c797485a8fe5c80335f5df39c16278ce5cb0
f.sousa <ng.dev.sousa@outlook.com>
2021-03-28 20:35:38 -0300

    Add delete_user_by_id to UsersController

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,7 +1,9 @@
 defmodule Rockelivery do
   alias Rockelivery.Users.Create, as: UserCreate
+  alias Rockelivery.Users.Delete, as: DeleteUserByID
   alias Rockelivery.Users.Get, as: GetUserByID
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate delete_user_by_id(id), to: DeleteUserByID, as: :call
   defdelegate get_user_by_id(id), to: GetUserByID, as: :by_id
 end
