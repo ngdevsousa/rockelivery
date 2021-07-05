d320f38f1a3622fdb510a5df70fa4276fad4de90
f.sousa <ng.dev.sousa@outlook.com>
2021-07-05 19:58:34 -0300

    Add Create Order

--- lib/rockelivery/order.ex
+++ lib/rockelivery/order.ex
@@ -6,11 +6,13 @@ defmodule Rockelivery.Order do
   alias Rockelivery.{Item, User}
 
   @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
   @payment_methods [:money, :credit_card, :debit_card]
   @required_params [:address, :comments, :payment_method, :user_id]
   @derive {Jason.Encoder, only: @required_params ++ [ :id ]}
 
-  schema "items" do
+  schema "orders" do
     field :address, :string
     field :comments, :string
     field :payment_method, Ecto.Enum, values: @payment_methods
@@ -21,10 +23,10 @@ defmodule Rockelivery.Order do
     timestamps()
   end
 
-  def changeset(struct \\ %__MODULE__{}, required_fields, params, items) do
+  def changeset(struct \\ %__MODULE__{}, params, items) do
     struct
-    |> cast(params, required_fields)
-    |> validate_required(required_fields)
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
     |> put_assoc(:items, items)
   end
 end
