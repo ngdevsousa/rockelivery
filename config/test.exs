638f6f831d78306d8ea472e0645dfd5ab627c78b
f.sousa <ng.dev.sousa@outlook.com>
2021-07-18 18:11:01 -0300

    Update env

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # Run `mix help test` for more information.
 config :rockelivery, Rockelivery.Repo,
   username: "postgres",
-  password: "p@ssw0rd",
+  password: "postgres",
   database: "rockelivery_test#{System.get_env("MIX_TEST_PARTITION")}",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
